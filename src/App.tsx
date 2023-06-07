032a4fa01fe855add150692945f032933f3e3337
TamasSallai <tamas.sallai.hu@gmail.com>
2023-06-07 11:07:28 +0200

    added firebase fucntions for columns, deleted unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import './App.css'
-import { createTable } from './firebase'
-
 function App() {
-  const handleClick = async () => {
-    await createTable('Hello World')
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick}>Create table</button>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,12 @@
 import { initializeApp } from 'firebase/app'
-import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import {
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  setDoc,
+} from 'firebase/firestore'
+import { v4 as uuidv4 } from 'uuid'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,8 +21,31 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-export const createTable = async (tableName: string) => {
-  await addDoc(collection(db, 'tables'), {
+export const addTable = async (tableName: string) => {
+  const tableId = uuidv4()
+  await setDoc(doc(db, 'tables', tableId), {
+    id: tableId,
     name: tableName,
   })
+}
+
+export const getTables = async () => {
+  const querySnapshot = await getDocs(collection(db, 'tables'))
+  return querySnapshot.docs.map((doc) => doc.data())
+}
+
+export const addColumnToTable = async (tableId: string, columnName: string) => {
+  const columnId = uuidv4()
+  await setDoc(doc(db, `tables`, tableId, 'columns', columnId), {
+    id: columnId,
+    name: columnName,
+  })
+}
+
+export const getColumnsForTable = async (tableId: string) => {
+  const querySnapshot = await getDocs(
+    collection(db, 'tables', tableId, 'columns')
+  )
+
+  return querySnapshot.docs.map((doc) => doc.data())
 }
