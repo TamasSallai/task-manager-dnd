134398708f5d52d9132c004969295bcccd48bdc8
TamasSallai <tamas.sallai.hu@gmail.com>
2023-06-19 19:33:03 +0200

    component structuring and design

---
+++ src/components/SortableTask/SortableTask.tsx
@@ -0,0 +1,48 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import './SortableTask.css'
+
+type Props = {
+  id: string
+  name: string
+}
+
+const SortableTask = ({ id, name }: Props) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className="sortable-task"
+    >
+      <p className="sortable-task-name">{name}</p>
+
+      <button className="button-with-icon" data-no-dnd="true">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default SortableTask
